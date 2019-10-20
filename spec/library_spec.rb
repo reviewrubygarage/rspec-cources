5490a0dd14edeeb99af440943e731783b0c03613
Alex Bal <differencialx@gmail.com>
2019-10-21 02:37:49 +0300

    Use let

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,31 +1,27 @@
 require 'library'
 
 RSpec.describe Library do
-  before(:each) do
-    @library = Library.new
-  end
+  let(:library) { Library.new }
 
   describe '.new' do
     it 'initializes empty Library' do
-      expect(@library.books.size).to eq 0
+      expect(library.books.size).to eq 0
     end
 
-    specify { expect(@library.books).to be_a Array }
+    specify { expect(library.books).to be_a Array }
   end
 
   describe '#add_book' do
-    before(:all) do
-      @book_name = 'First book'
-    end
+    let(:book_name) { 'First book' }
 
     it 'adds book with name to library' do
-      @library.add_book(name: @book_name)
-      expect(@library.books).to match([{name: @book_name}])
+      library.add_book(name: book_name)
+      expect(library.books).to match([{name: book_name}])
     end
 
     it 'increases books count by 1' do
-      expect { @library.add_book(name: @book_name) }.to(
-        change{ @library.books.size }.from(0).to(1)
+      expect { library.add_book(name: book_name) }.to(
+        change{ library.books.size }.from(0).to(1)
       )
     end
   end
