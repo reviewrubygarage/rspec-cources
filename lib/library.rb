db5f52496ec4894e854e53068dead71d018bec9f
Alex Bal <differencialx@gmail.com>
2019-11-10 20:56:38 +0200

    Allow to print library books out

--- lib/library.rb
+++ lib/library.rb
@@ -1,4 +1,6 @@
 class Library
+  EMPTY_LIBRARY_MESSAGE = 'Library is empty'.freeze
+
   attr_reader :books
 
   def initialize(books = [])
@@ -7,5 +9,13 @@ class Library
 
   def add_book(name:)
     books << { name: name }
+  end
+
+  def show_books
+    return EMPTY_LIBRARY_MESSAGE if books.empty?
+
+    books.each_with_index.map do |book, index|
+      "#{index + 1}. #{book[:name]}"
+    end.join("\n")
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,7 +1,8 @@
 require 'library'
 
 RSpec.describe Library do
-  subject(:library) { described_class.new }
+  subject(:library) { described_class.new(books) }
+  let(:books) { [] }
 
   describe '.new' do
     it 'initializes empty Library' do
@@ -23,6 +24,31 @@ RSpec.describe Library do
       expect { library.add_book(name: book_name) }.to(
         change{ library.books.size }.from(0).to(1)
       )
+    end
+  end
+
+  describe '#show_books' do
+    context 'empty Library' do
+      let(:expected_message) { 'Library is empty' }
+
+      it 'returns empty message' do
+        expect(subject.show_books).to eq expected_message
+      end
+    end
+
+    context 'Library with books' do
+      let(:expected_message) do
+        "1. #{book_name_1}"\
+        "\n"\
+        "2. #{book_name_2}"
+      end
+      let(:book_name_1) { 'Book 1' }
+      let(:book_name_2) { 'Book 2' }
+      let(:books) { [{ name: book_name_1 }, { name: book_name_2 }] }
+
+      it 'returns a list of books' do
+        expect(subject.show_books).to eq expected_message
+      end
     end
   end
 end
