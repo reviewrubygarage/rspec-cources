44bb2ca2d2bbddf35a32947d0098543185d6c19c
Alex Bal <differencialx@gmail.com>
2019-11-10 19:24:18 +0200

    Use true/false matcher

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Library do
 
   describe '.new' do
     it 'initializes empty Library' do
-      expect(library.books.size).to eq 0
+      expect(library.books.empty?).to be true
     end
 
     specify { expect(library.books).to be_a Array }
