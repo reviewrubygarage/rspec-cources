3b32feba39cfd244f380c282ae468198d9a5afc1
Alex Bal <differencialx@gmail.com>
2019-11-19 00:13:55 +0200

    Allow to quit from console

--- lib/console.rb
+++ lib/console.rb
@@ -7,20 +7,38 @@ class Console
 
   def run
     greeting
-    show_options
+    loop do
+      break unless choose_option
+    end
+    goodbye
   end
 
   private
+
+  def choose_option
+    show_options
+    input = read_input
+    return false if input == 'quit'
+  end
 
   def show_options
     puts "Please choose an option\n"\
     "add - add new book to library\n"\
     "remove - remove book from library\n"\
     "show - show available books in library\n"\
-    'exit - to exit library'
+    'quit - to quit library'
   end
 
   def greeting
     puts 'Hello'
+    @state = :choose_option
+  end
+
+  def goodbye
+    puts 'Goodbye'
+  end
+
+  def read_input
+    STDIN.gets.chomp
   end
 end
--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Console do
   subject(:console) { described_class.new.run }
 
   describe 'run' do
+    let(:input_sequence) { %w[quit] }
+    before do
+      allow(STDIN).to receive(:gets).and_return(*input_sequence)
+    end
+
     context 'greeting message' do
       it 'prints Hello' do
         expect{ console }.to output(/Hello/).to_stdout
@@ -21,6 +26,10 @@ RSpec.describe Console do
 
       it 'prints options message' do
         expect { console }.to output(/#{options_message}/).to_stdout
+      end
+
+      it 'quits from console app when user types quit' do
+        expect { console }.to output(/Goodbye/).to_stdout
       end
     end
   end
